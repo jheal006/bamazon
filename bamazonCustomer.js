8698504f4a05d1509b724bd305b3ca6d4d03f31b
jheal006 <jheal006@gmail.com>
2017-09-20 22:31:05 -0700

    Functional Basic Assignment

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 // List Required Nodes / JS files / Variables
 var mysql = require("mysql");
 var inquirer = require('inquirer');
+require("console.table");
 // Decorative Var for Console
 var lineBreak = "==========================================";
 
@@ -15,63 +16,87 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-// Connect to the mysql server and sql database
-// connection.connect(function(err) {
-//   if (err) throw err;
-//   // run the start function after the connection is made to prompt the user
-//   start();
-// });
-
-// Var to check and see if product does in fact exist
-var isProduct = function(){
-    connection.query("SELECT item_id FROM products",
-    function(err){
+//Load product table
+function loadProducts() {
+  connection.query("SELECT * FROM products",
+    function (err, res) {
       if (err) throw err;
-      console.log(results);
-    }
-    );
-  };
-
+      console.table(res);
+      orders();
+    });
+};
 
 // function which first prompts the user for which item they would like to purchase
-function start() {
+function orders() {
   inquirer
     .prompt([
-    {
-      name: "productID",
-      type: "input",
-      message: "Please input the productID for the item you would like to purchase: \n",
-      validate: function(value) {
-        if (isNaN(value) === false) {
-          return true;
+      {
+        name: "productID",
+        type: "input",
+        message: "Please input the productID for the item you would like to purchase: \n",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          console.log("Please enter a item ID!");
+          return false;
         }
+      },
+      {
+        name: "units",
+        type: "input",
+        message: "How many units would you like to purchase? \n",
+        validate: function (value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+        console.log("Please enter a item ID!");
         return false;
-      }
-    },
-    {
-    name: "units",
-    type: "input",
-    message: "How many units would you like to purchase? \n",
-    validate: function(value) {
-      if (isNaN(value) === false) {
-        return true;
-      }
-      return false;
-    }
-  }])
-    .then(function(answers) {
-      // if the entered productID matches a product then reduce by the answers.units
-      var query = "SELECT stock_quantity FROM products WHERE item_id = '2'";
-      connection.query(query,
-      function(err, res) {
-          console.log(res[0].stock_quantity);
-          // // re-prompt the user for if they want to bid or post
-          // start();
-          console.log(answers.units)
         }
-      );
+      }])
+    // After collecting Product ID from User and Quantity of items, check to make sure there are enough items in stock, and update DB accordingly
+    .then(function(answers) {
+      var id = answers.productID;
+      // if the entered productID matches a product then reduce by the answers.units
+      var querySearch = "SELECT * FROM products WHERE ?";
+      var queryUpdate = "UPDATE products SET ? WHERE ?";
+      connection.query(querySearch, { item_id: id },
+        function (err, res) {
+          var quantity = res[0].stock_quantity;
+          if (quantity > answers.units) {
+            quantity = quantity - answers.units;
+            // console.log("QUANTITY ========= ", quantity);
+            // console.log("response ========", res);
+            connection.query(
+              queryUpdate,
+              [
+                {
+                 stock_quantity: quantity
+                },
+                {
+                  item_id: id
+                }
+              ],
+              function(error) {
+                if (error) throw err;
+                console.log("Bid placed successfully!");
+                var itemCost = answers.units * res[0].price;
+                console.log("Your order total is $" + itemCost);
+                // loadProducts();
+                // orders();
+              }
+            );
+          } else {
+            console.log('Not enough items in stock!');
+            orders();
+          }
+          // console.log(res);
+          // // re-prompt the user for if they want to bid or post
+          // console.log(answers.units)
+      });
       // console.log('Sold!');
     });
 }
 
-start();
+// Load list and Array of Products
+loadProducts();
